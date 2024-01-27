83659f91b3ffd6b6da2f77464cb9daeb8c355509
utsavchab <21je1011@iitism.ac.in>
2024-01-27 06:43:37 +0530

    python server connected

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
 const test = require('./test/test')
+const dotenv = require('dotenv')
+const axios = require('axios')
+
+dotenv.config()
 
 app.use(
   cors({
@@ -15,29 +19,67 @@ app.use(
 app.use(express.json({ limit: '100mb' }))
 app.use(morgan('dev'))
 
+const getTextData = ($) => {
+  const textWithId = []
+  let idCounter = 1
+  $('div').each((index, element) => {
+    const divElm = $(element)
+    const text = divElm.text().trim()
+    if (divElm.find('div').length === 0 && text.length > 0) {
+      //it should not have any child div and text length should be greater then 0.
+      textWithId.push({
+        text,
+        class: divElm.attr('class'),
+      })
+    }
+    idCounter++
+  })
+  return textWithId
+}
+
 app.post('/', async (req, res) => {
-  const htmlString = req.body // Assuming the HTML string is in the request body
-  const $ = cheerio.load(htmlString.data)
-  const title = $('title').text()
-  let fileName = `${title}`.replace(/[<>:"/\\|?*]/g, '_').trim() // Replace invalid characters with underscore
+  const { htmlString } = req.body
 
-  if (fileName.length > 20) {
-    fileName = fileName.substring(0, 20) // Take only the first 20 characters
-  }
-  fileName += '.html'
+  const $ = cheerio.load(htmlString)
+  $('script').remove()
+  $('noscript').remove()
+  $('style').remove()
 
-  fs.writeFileSync(path.join(__dirname, 'html', fileName), $.html(), 'utf-8')
-  console.log(fileName)
-  res.json({ success: 'true' })
+  const textData = getTextData($)
+  const {
+    data: { data: patternsData },
+  } = await axios.post(`${process.env.PYTHON_SERVER}/api/v1/predict`, {
+    data: textData,
+  })
+
+  // const title = $('title').text()
+  // let fileName = `${title}`.replace(/[<>:"/\\|?*]/g, '_').trim() // Replace invalid characters with underscore
+
+  // if (fileName.length > 20) {
+  //   fileName = fileName.substring(0, 20) // Take only the first 20 characters
+  // }
+  // fileName += '.html'
+
+  // fs.writeFileSync(path.join(__dirname, 'html', fileName), $.html(), 'utf-8')
+  // console.log(fileName)
+  console.log(patternsData)
+  res.json({ success: 'true', data: patternsData })
 })
 
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
-app.get('/test', (req, res) => {
-  test()
-  res.send('test success')
+app.get('/test', async (req, res) => {
+  try {
+    const data = await test()
+    res.status(200).send(data)
+  } catch (err) {
+    // console.log(err.response.data)
+    res.status(400).json({
+      message: err?.response?.data?.error,
+    })
+  }
 })
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
--- src/test/test.js
+++ src/test/test.js
@@ -1,77 +1,39 @@
 const cheerio = require('cheerio')
 const path = require('path')
 const fs = require('fs')
+const axios = require('axios')
 
-const getLocation = (node) => {
-  const locationParts = []
+// const getLocation = (node) => {
+//   const locationParts = []
 
-  while (node && node.type === 'tag' && node.name !== 'html') {
-    let locationPart = node.name
-    if (node.attribs.class) {
-      locationPart += `.${node.attribs.class.split(' ').join('.')}`
-    }
-    const siblings = node.parent.children.filter(
-      (child) => child.type === 'tag' && child.name === node.name
-    )
-    if (siblings.length > 1) {
-      const index = siblings.indexOf(node) + 1
-      locationPart += `:nth-child(${index})`
-    }
-    locationParts.push(locationPart)
-    node = node.parent
-  }
+//   while (node && node.type === 'tag' && node.name !== 'html') {
+//     let locationPart = node.name
+//     if (node.attribs.class) {
+//       locationPart += `.${node.attribs.class.split(' ').join('.')}`
+//     }
+//     const siblings = node.parent.children.filter(
+//       (child) => child.type === 'tag' && child.name === node.name
+//     )
+//     if (siblings.length > 1) {
+//       const index = siblings.indexOf(node) + 1
+//       locationPart += `:nth-child(${index})`
+//     }
+//     locationParts.push(locationPart)
+//     node = node.parent
+//   }
 
-  return locationParts.reverse().join(' > ')
-}
-
-const getTextNodes = ($, parentNode) => {
-  const textNodes = []
-
-  const traverse = (node) => {
-    if (node.type === 'text' && node.data.trim() !== '') {
-      textNodes.push({
-        text: node.data.trim(),
-        location: getLocation(node.parent),
-      })
-    } else if (node.children) {
-      for (const childNode of node.children) {
-        traverse(childNode)
-      }
-    }
-  }
-
-  traverse(parentNode)
-
-  return textNodes
-}
+//   return locationParts.reverse().join(' > ')
+// }
 
 // const getTextNodes = ($, parentNode) => {
 //   const textNodes = []
 
 //   const traverse = (node) => {
-//     console.log('Node:', node)
-
 //     if (node.type === 'text' && node.data.trim() !== '') {
 //       textNodes.push({
 //         text: node.data.trim(),
 //         location: getLocation(node.parent),
 //       })
-//       console.log('Added text:', node.data.trim())
-//     } else if (
-//       node.type === 'tag' &&
-//       ['style', 'script', 'noscript'].includes(node.name)
-//     ) {
-//       console.log('Skipping node:', node.name)
-//       // Skip style, script, and noscript tags
-//       return
-//     } else if (
-//       node.type === 'tag' &&
-//       node.attribs &&
-//       (node.attribs.style || node.attribs.class || node.attribs.id)
-//     ) {
-//       console.log('Skipping node with attributes:', node.attribs)
-//       // Skip nodes with inline styles, classes, or IDs
-//       return
 //     } else if (node.children) {
 //       for (const childNode of node.children) {
 //         traverse(childNode)
@@ -84,86 +46,125 @@ const getTextNodes = ($, parentNode) => {
 //   return textNodes
 // }
 
-const getElementByLocation = ($, location) => {
-  return $(location).text()
-}
+// // const getTextNodes = ($, parentNode) => {
+// //   const textNodes = []
 
-const getBodyTextWithLine = ($, bodyNode) => {
-  let bodyTextNodes = []
-  let lineNumber = 0
+// //   const traverse = (node) => {
+// //     console.log('Node:', node)
 
-  const traverseNodes = (node) => {
-    if (node.type === 'text') {
-      const text = node.data.trim()
-      if (text !== '') {
-        bodyTextNodes.push({
-          text,
-          lineNumber: ++lineNumber,
-        })
-      }
-    } else if (node.type === 'tag') {
-      lineNumber++
-      node.children.forEach(traverseNodes)
-    }
-  }
+// //     if (node.type === 'text' && node.data.trim() !== '') {
+// //       textNodes.push({
+// //         text: node.data.trim(),
+// //         location: getLocation(node.parent),
+// //       })
+// //       console.log('Added text:', node.data.trim())
+// //     } else if (
+// //       node.type === 'tag' &&
+// //       ['style', 'script', 'noscript'].includes(node.name)
+// //     ) {
+// //       console.log('Skipping node:', node.name)
+// //       // Skip style, script, and noscript tags
+// //       return
+// //     } else if (
+// //       node.type === 'tag' &&
+// //       node.attribs &&
+// //       (node.attribs.style || node.attribs.class || node.attribs.id)
+// //     ) {
+// //       console.log('Skipping node with attributes:', node.attribs)
+// //       // Skip nodes with inline styles, classes, or IDs
+// //       return
+// //     } else if (node.children) {
+// //       for (const childNode of node.children) {
+// //         traverse(childNode)
+// //       }
+// //     }
+// //   }
 
-  traverseNodes(bodyNode)
+// //   traverse(parentNode)
 
-  return bodyTextNodes
-}
+// //   return textNodes
+// // }
 
-const addSyntheticIds = ($) => {
-  let idCounter = 0
+// const getElementByLocation = ($, location) => {
+//   return $(location).text()
+// }
 
-  const generateId = () => {
-    return `id_${++idCounter}`
-  }
+// const getBodyTextWithLine = ($, bodyNode) => {
+//   let bodyTextNodes = []
+//   let lineNumber = 0
 
-  const traverseAndAssignIds = (element) => {
-    if (
-      element.type === 'tag' &&
-      !['script', 'style', 'noscript'].includes(element.name)
-    ) {
-      $(element).attr('id', generateId())
-    }
-    element?.children?.forEach((child) => {
-      traverseAndAssignIds(child)
-    })
-  }
+//   const traverseNodes = (node) => {
+//     if (node.type === 'text') {
+//       const text = node.data.trim()
+//       if (text !== '') {
+//         bodyTextNodes.push({
+//           text,
+//           lineNumber: ++lineNumber,
+//         })
+//       }
+//     } else if (node.type === 'tag') {
+//       lineNumber++
+//       node.children.forEach(traverseNodes)
+//     }
+//   }
 
-  const body = $('body')[0]
-  if (body) {
-    traverseAndAssignIds(body)
-  }
-}
+//   traverseNodes(bodyNode)
 
-const getTextsWithIds = ($) => {
-  const textObjects = []
+//   return bodyTextNodes
+// }
 
-  const traverseAndPushText = (element) => {
-    if (
-      element.type === 'tag' &&
-      element.children.length === 1 &&
-      element.children[0].type === 'text'
-    ) {
-      const text = element.children[0].data.trim()
-      if (text !== '') {
-        const id = $(element).attr('id')
-        textObjects.push({ text, id })
-      }
-    }
-    element?.children?.forEach((child) => {
-      traverseAndPushText(child)
-    })
-  }
+// const addSyntheticIds = ($) => {
+//   let idCounter = 0
 
-  const body = $('body')[0]
-  if (body) {
-    traverseAndPushText(body)
-  }
+//   const generateId = () => {
+//     return `id_${++idCounter}`
+//   }
 
-  return textObjects
-}
+//   const traverseAndAssignIds = (element) => {
+//     if (
+//       element.type === 'tag' &&
+//       !['script', 'style', 'noscript'].includes(element.name)
+//     ) {
+//       $(element).attr('id', generateId())
+//     }
+//     element?.children?.forEach((child) => {
+//       traverseAndAssignIds(child)
+//     })
+//   }
+
+//   const body = $('body')[0]
+//   if (body) {
+//     traverseAndAssignIds(body)
+//   }
+// }
+
+// const getTextsWithIds = ($) => {
+//   const textObjects = []
+
+//   const traverseAndPushText = (element) => {
+//     if (
+//       element.type === 'tag' &&
+//       element.children.length === 1 &&
+//       element.children[0].type === 'text'
+//     ) {
+//       const text = element.children[0].data.trim()
+//       if (text !== '') {
+//         const id = $(element).attr('id')
+//         textObjects.push({ text, id })
+//       }
+//     }
+//     element?.children?.forEach((child) => {
+//       traverseAndPushText(child)
+//     })
+//   }
+
+//   const body = $('body')[0]
+//   if (body) {
+//     traverseAndPushText(body)
+//   }
+
+//   return textObjects
+// }
 
 const getTextData = ($) => {
   const textWithId = []
@@ -183,7 +184,7 @@ const getTextData = ($) => {
   return textWithId
 }
 
-const test = () => {
+const test = async () => {
   const htmlFile = fs.readFileSync(
     path.join(__dirname, '..', 'html', 'Amazon.in_ Buy Lenov.html'),
     'utf-8'
@@ -194,14 +195,21 @@ const test = () => {
   $('style').remove()
 
   const textData = getTextData($)
-  const jsonData = JSON.stringify(textData, null, 2)
-  const outputPath = path.join(__dirname, 'textData.json')
-  console.log(getElementByLocation($, `.id_940`))
-  fs.writeFileSync(outputPath, jsonData)
+  // const jsonData = JSON.stringify(textData, null, 2)
+  // const outputPath = path.join(__dirname, 'textData.json')
+  // console.log(getElementByLocation($, `.id_940`))
+  // fs.writeFileSync(outputPath, jsonData)
 
   // send this jsonData to python server.
+  console.log(`${process.env.PYTHON_SERVER}/api/v1/predict`)
+  const { data } = await axios.post(
+    `${process.env.PYTHON_SERVER}/api/v1/predict`,
+    {
+      data: textData,
+    }
+  )
 
-  return true
+  return data
 }
 
 module.exports = test
